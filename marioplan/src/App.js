3e350ea7a2e6e8111644a94454b7f1c0099f1dd1
Tyler Sweeney <tylersweeney@gmail.com>
2019-01-27 11:50:30 -0800

    create signin component

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/dashboard'
-import ProjectDetails from './components/projects/projectdetails'
+import Dashboard from './components/dashboard/dashboard';
+import ProjectDetails from './components/projects/projectdetails';
+import SignIn from './components/auth/signin'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
           </Switch>
         </div>
       </BrowserRouter>
