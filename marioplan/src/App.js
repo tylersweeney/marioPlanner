b67e08abb092e4b2eda54243ccfd74e57df6a058
Tyler Sweeney <tylersweeney@gmail.com>
2019-01-27 12:15:36 -0800

    add sign up component and route to app.js

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/dashboard';
 import ProjectDetails from './components/projects/projectdetails';
 import SignIn from './components/auth/signin'
+import SignUp from './components/auth/signup'
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
+            <Route path ='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
