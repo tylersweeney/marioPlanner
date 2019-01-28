a0a516a795db4e5a37e0f318d87fb05a826a9dac
Tyler Sweeney <tylersweeney@gmail.com>
2019-01-27 19:38:44 -0800

    Create ProjectActions component to handle asynchronous responses

--- marioplan/src/components/projects/createproject.js
+++ marioplan/src/components/projects/createproject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 export class CreateProject extends Component {
     state ={
@@ -12,7 +14,7 @@ export class CreateProject extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.createProject(this.state)
     }
   render() {
     return (
@@ -37,5 +39,10 @@ export class CreateProject extends Component {
     )
   }
 }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
 
-export default CreateProject
+export default connect(null, mapDispatchToProps)(CreateProject)
