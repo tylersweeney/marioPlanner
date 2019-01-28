5d1830614704f400534ff98da81348f7b295bc9c
Tyler Sweeney <tylersweeney@gmail.com>
2019-01-27 23:17:30 -0800

    added firebaseauthisready

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -4,21 +4,22 @@ import SignedInLinks from './signedinlinks'
 import SignedOutLinks from './signedoutlinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props
+    // console.log(auth)
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to= '/' className="left brand-logo">MarioPlan</Link>
-                <SignedInLinks/>
-                <SignedOutLinks/>
+                { links }
             </div>
         </nav>   
     )
 }
 const mapStateToProps = (state) => {
-    console.log(state)
     return{
-
+        auth: state.firebase.auth
     }
 }
 
