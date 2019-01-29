35891f319c7be9fcff0615598c4a55952b58c136
Tyler Sweeney <tylersweeney@gmail.com>
2019-01-28 18:53:54 -0800

    add notifications component

--- marioplan/src/components/dashboard/dashboard.js
+++ marioplan/src/components/dashboard/dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom'
 class Dashboard extends Component {
     render() {
         //console.log(this.props)
-        const {projects, auth } = this.props;
+        const {projects, auth, notifications } = this.props;
         if (!auth.uid) return <Redirect to='/signin' />
         return(
             <div className="dashboard container">
@@ -20,7 +20,7 @@ class Dashboard extends Component {
                     </div>
                     {/* div for notifications on the right */}
                     <div className="col s12 m5 offest-m1">
-                        <Notifications/>
+                    <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -32,13 +32,15 @@ const mapStateToProps = (state) => {
     console.log(state);
     return{
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects', orderBy: ['createdAt', 'desc']},
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
     ])
     )(Dashboard);
